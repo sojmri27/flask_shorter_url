282b91f6ee17975c50766476b07e5fb9c8770eaf
Nina <nina.hu@alpha-info.com.tw>
2020-02-21 10:01:10 +0800

    max data coun

--- main.py
+++ main.py
@@ -6,18 +6,25 @@ import pandas as pd
 app = Flask(__name__)
 
 def short(url):
-    data_file = open('db.txt', "r+")
+    data_file = open('db.txt', "r")
     data_str = data_file.read()
+    data_file.close()
     rows = data_str.split('\n')
     df_data = pd.DataFrame([] if len(rows)==1 else [row.split(', ') for row in rows[1:]], columns=['url', 'short_url'])
     list_url = df_data.index[df_data['url'] == url].tolist()
     if len(list_url)>0:
-        data_file.close()
         return df_data.iloc[list_url[0]]['short_url']
+    prime = 400651    
     d_id = len(df_data)
-    short_url = base64.b64encode(hashlib.md5((str(d_id)).encode('utf-8')).digest(), altchars=b"-_")[:6].decode("utf-8")
-    data_file.write('\n'+url+', '+short_url)
-    data_file.close()
+    real_id = d_id%prime
+    short_url = base64.b64encode(hashlib.md5((str(real_id)).encode('utf-8')).digest(), altchars=b"-_")[:6].decode("utf-8")
+    if d_id>=prime:
+      rows[real_id+1] = url+', '+short_url
+    else:
+      rows.append(url+', '+short_url)
+    db_file = open('db.txt', "w")
+    db_file.write('\n'.join(rows))
+    db_file.close()
     return short_url
 
 
